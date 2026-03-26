66c09cdc59c66f5956edc904580b53a1954a1b4f
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2026-03-26 22:24:48 +0900

    [Gold V] Title: 블랙 프라이데이, Time: 452 ms, Memory: 15668 KB -BaekjoonHub

---
+++ 백준/Gold/18114. 블랙 프라이데이/블랙 프라이데이.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int [] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        if(Arrays.binarySearch(arr,c)>=0){
+            System.out.println(1);
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if(arr[i]>c)break;
+            int temp = c-arr[i];
+            int rest = Arrays.binarySearch(arr,temp);
+            if(rest>=0&&arr[rest]!=arr[i]){
+                System.out.println(1);
+                return;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if(arr[i]>c)break;
+            for (int j = 0; j < n; j++) {
+                if(arr[j]==arr[i]){continue;}
+                if(arr[i]+arr[j]>c)break;
+                int temp = c-arr[i]-arr[j];
+                int rest = Arrays.binarySearch(arr,temp);
+                if(rest>=0 && arr[rest]!=arr[i] && arr[rest]!=arr[j]){
+                    System.out.println(1);
+                    return;
+                }
+            }
+        }
+        System.out.println(0);
+    }
+}
