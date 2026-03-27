d2606f7b2932b101e0713423ef81fa9998c66ab2
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2026-03-27 22:30:42 +0900

    [Bronze V] Title: Rectangles, Time: 100 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/15232. Rectangles/Rectangles.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int c =  Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < c; j++) {
+                sb.append("*");
+            }sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
